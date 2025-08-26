3a995f2d72b78476fbf130462cba1e568f6bc1f5
Ruisz1 <jpenggu@gmail.com>
2025-08-26 18:47:08 +0800

    historyyyyyyyy

--- Deped Tracking System/deptHistory.vb
+++ Deped Tracking System/deptHistory.vb
@@ -1,3 +1,16 @@
-﻿Public Class deptHistory
+﻿Imports System.Data.OleDb
+Imports System.Diagnostics
+Imports System.Drawing.Imaging
+Imports System.Drawing.Printing
+Imports System.IO
+Imports System.Windows.Controls
+Imports FontAwesome.Sharp
+Public Class deptHistory
+    Private Sub deptHistory_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
+    End Sub
+
+    Private Sub Guna2TextBox2_TextChanged(sender As Object, e As EventArgs) Handles Guna2TextBox2.TextChanged
+
+    End Sub
 End Class
--- Deped Tracking System/sysModule.vb
+++ Deped Tracking System/sysModule.vb
@@ -5,4 +5,5 @@ Module sysModule
     Private dbPath As String = Path.Combine(Application.StartupPath, "Database\Deped.accdb")
 
     Public conString As String = $"Provider=Microsoft.ACE.OLEDB.12.0;Data Source={Path.GetFullPath(dbPath)};"
+
 End Module
