f6c993e6509f0ed0271dd3b3b977691fd885c7a8
Evan Arenas <jibbyarenas@gmail.com>
2025-08-27 11:22:32 +0800

    send all

--- Deped Tracking System/deptChecklist.Designer.vb
+++ Deped Tracking System/deptChecklist.Designer.vb
@@ -26,15 +26,15 @@ Partial Class deptChecklist
         Me.Guna2Separator1 = New Guna.UI2.WinForms.Guna2Separator()
         Me.btnCreate = New Guna.UI2.WinForms.Guna2Button()
         Me.Guna2Panel1 = New Guna.UI2.WinForms.Guna2Panel()
+        Me.btnReceiveAll = New Guna.UI2.WinForms.Guna2Button()
         Me.flpPending = New System.Windows.Forms.FlowLayoutPanel()
         Me.flpChecklist = New System.Windows.Forms.FlowLayoutPanel()
         Me.Guna2CustomGradientPanel2 = New Guna.UI2.WinForms.Guna2CustomGradientPanel()
-        Me.btnReceiveAll = New Guna.UI2.WinForms.Guna2Button()
+        Me.btnSendAll = New Guna.UI2.WinForms.Guna2Button()
+        Me.cmbSort = New Guna.UI2.WinForms.Guna2ComboBox()
         Me.btnRefresh = New FontAwesome.Sharp.IconButton()
         Me.chkSelect = New System.Windows.Forms.CheckBox()
         Me.IconPictureBox1 = New FontAwesome.Sharp.IconPictureBox()
-        Me.cmbSort = New Guna.UI2.WinForms.Guna2ComboBox()
-        Me.btnSendAll = New Guna.UI2.WinForms.Guna2Button()
         Me.Guna2Panel1.SuspendLayout()
         Me.Guna2CustomGradientPanel2.SuspendLayout()
         CType(Me.IconPictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
@@ -103,6 +103,22 @@ Partial Class deptChecklist
         Me.Guna2Panel1.Size = New System.Drawing.Size(418, 962)
         Me.Guna2Panel1.TabIndex = 6
         '
+        'btnReceiveAll
+        '
+        Me.btnReceiveAll.BorderRadius = 20
+        Me.btnReceiveAll.DisabledState.BorderColor = System.Drawing.Color.DarkGray
+        Me.btnReceiveAll.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray
+        Me.btnReceiveAll.DisabledState.FillColor = System.Drawing.Color.FromArgb(CType(CType(169, Byte), Integer), CType(CType(169, Byte), Integer), CType(CType(169, Byte), Integer))
+        Me.btnReceiveAll.DisabledState.ForeColor = System.Drawing.Color.FromArgb(CType(CType(141, Byte), Integer), CType(CType(141, Byte), Integer), CType(CType(141, Byte), Integer))
+        Me.btnReceiveAll.FillColor = System.Drawing.Color.Transparent
+        Me.btnReceiveAll.Font = New System.Drawing.Font("Segoe UI", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnReceiveAll.ForeColor = System.Drawing.Color.FromArgb(CType(CType(36, Byte), Integer), CType(CType(35, Byte), Integer), CType(CType(34, Byte), Integer))
+        Me.btnReceiveAll.Location = New System.Drawing.Point(295, 12)
+        Me.btnReceiveAll.Name = "btnReceiveAll"
+        Me.btnReceiveAll.Size = New System.Drawing.Size(111, 40)
+        Me.btnReceiveAll.TabIndex = 20
+        Me.btnReceiveAll.Text = "Receive All"
+        '
         'flpPending
         '
         Me.flpPending.AutoScroll = True
@@ -144,21 +160,40 @@ Partial Class deptChecklist
         Me.Guna2CustomGradientPanel2.Size = New System.Drawing.Size(906, 47)
         Me.Guna2CustomGradientPanel2.TabIndex = 65
         '
-        'btnReceiveAll
+        'btnSendAll
         '
-        Me.btnReceiveAll.BorderRadius = 20
-        Me.btnReceiveAll.DisabledState.BorderColor = System.Drawing.Color.DarkGray
-        Me.btnReceiveAll.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray
-        Me.btnReceiveAll.DisabledState.FillColor = System.Drawing.Color.FromArgb(CType(CType(169, Byte), Integer), CType(CType(169, Byte), Integer), CType(CType(169, Byte), Integer))
-        Me.btnReceiveAll.DisabledState.ForeColor = System.Drawing.Color.FromArgb(CType(CType(141, Byte), Integer), CType(CType(141, Byte), Integer), CType(CType(141, Byte), Integer))
-        Me.btnReceiveAll.FillColor = System.Drawing.Color.Transparent
-        Me.btnReceiveAll.Font = New System.Drawing.Font("Segoe UI", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.btnReceiveAll.ForeColor = System.Drawing.Color.FromArgb(CType(CType(36, Byte), Integer), CType(CType(35, Byte), Integer), CType(CType(34, Byte), Integer))
-        Me.btnReceiveAll.Location = New System.Drawing.Point(295, 12)
-        Me.btnReceiveAll.Name = "btnReceiveAll"
-        Me.btnReceiveAll.Size = New System.Drawing.Size(111, 40)
-        Me.btnReceiveAll.TabIndex = 20
-        Me.btnReceiveAll.Text = "Receive All"
+        Me.btnSendAll.BorderRadius = 20
+        Me.btnSendAll.DisabledState.BorderColor = System.Drawing.Color.DarkGray
+        Me.btnSendAll.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray
+        Me.btnSendAll.DisabledState.FillColor = System.Drawing.Color.FromArgb(CType(CType(169, Byte), Integer), CType(CType(169, Byte), Integer), CType(CType(169, Byte), Integer))
+        Me.btnSendAll.DisabledState.ForeColor = System.Drawing.Color.FromArgb(CType(CType(141, Byte), Integer), CType(CType(141, Byte), Integer), CType(CType(141, Byte), Integer))
+        Me.btnSendAll.FillColor = System.Drawing.Color.Transparent
+        Me.btnSendAll.Font = New System.Drawing.Font("Segoe UI", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnSendAll.ForeColor = System.Drawing.Color.FromArgb(CType(CType(36, Byte), Integer), CType(CType(35, Byte), Integer), CType(CType(34, Byte), Integer))
+        Me.btnSendAll.Location = New System.Drawing.Point(814, 3)
+        Me.btnSendAll.Name = "btnSendAll"
+        Me.btnSendAll.Size = New System.Drawing.Size(89, 40)
+        Me.btnSendAll.TabIndex = 21
+        Me.btnSendAll.Text = "Send All"
+        Me.btnSendAll.Visible = False
+        '
+        'cmbSort
+        '
+        Me.cmbSort.BackColor = System.Drawing.Color.Transparent
+        Me.cmbSort.BorderThickness = 0
+        Me.cmbSort.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed
+        Me.cmbSort.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cmbSort.FillColor = System.Drawing.Color.FromArgb(CType(CType(242, Byte), Integer), CType(CType(242, Byte), Integer), CType(CType(242, Byte), Integer))
+        Me.cmbSort.FocusedColor = System.Drawing.Color.FromArgb(CType(CType(94, Byte), Integer), CType(CType(148, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.cmbSort.FocusedState.BorderColor = System.Drawing.Color.FromArgb(CType(CType(94, Byte), Integer), CType(CType(148, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.cmbSort.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cmbSort.ForeColor = System.Drawing.Color.FromArgb(CType(CType(36, Byte), Integer), CType(CType(35, Byte), Integer), CType(CType(34, Byte), Integer))
+        Me.cmbSort.ItemHeight = 30
+        Me.cmbSort.Items.AddRange(New Object() {"Sort By", "Title (A-Z)", "Title (Z-A)", "Date (Newest)", "Date (Oldest)", "Client (A-Z)", "Client (Z-A)"})
+        Me.cmbSort.Location = New System.Drawing.Point(109, 6)
+        Me.cmbSort.Name = "cmbSort"
+        Me.cmbSort.Size = New System.Drawing.Size(151, 36)
+        Me.cmbSort.TabIndex = 21
         '
         'btnRefresh
         '
@@ -197,41 +232,6 @@ Partial Class deptChecklist
         Me.IconPictureBox1.Size = New System.Drawing.Size(21, 24)
         Me.IconPictureBox1.TabIndex = 3
         Me.IconPictureBox1.TabStop = False
-        '
-        'cmbSort
-        '
-        Me.cmbSort.BackColor = System.Drawing.Color.Transparent
-        Me.cmbSort.BorderThickness = 0
-        Me.cmbSort.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed
-        Me.cmbSort.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
-        Me.cmbSort.FillColor = System.Drawing.Color.FromArgb(CType(CType(242, Byte), Integer), CType(CType(242, Byte), Integer), CType(CType(242, Byte), Integer))
-        Me.cmbSort.FocusedColor = System.Drawing.Color.FromArgb(CType(CType(94, Byte), Integer), CType(CType(148, Byte), Integer), CType(CType(255, Byte), Integer))
-        Me.cmbSort.FocusedState.BorderColor = System.Drawing.Color.FromArgb(CType(CType(94, Byte), Integer), CType(CType(148, Byte), Integer), CType(CType(255, Byte), Integer))
-        Me.cmbSort.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.cmbSort.ForeColor = System.Drawing.Color.FromArgb(CType(CType(36, Byte), Integer), CType(CType(35, Byte), Integer), CType(CType(34, Byte), Integer))
-        Me.cmbSort.ItemHeight = 30
-        Me.cmbSort.Items.AddRange(New Object() {"Sort By", "Title (A-Z)", "Title (Z-A)", "Date (Newest)", "Date (Oldest)", "Client (A-Z)", "Client (Z-A)"})
-        Me.cmbSort.Location = New System.Drawing.Point(109, 6)
-        Me.cmbSort.Name = "cmbSort"
-        Me.cmbSort.Size = New System.Drawing.Size(151, 36)
-        Me.cmbSort.TabIndex = 21
-        '
-        'btnSendAll
-        '
-        Me.btnSendAll.BorderRadius = 20
-        Me.btnSendAll.DisabledState.BorderColor = System.Drawing.Color.DarkGray
-        Me.btnSendAll.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray
-        Me.btnSendAll.DisabledState.FillColor = System.Drawing.Color.FromArgb(CType(CType(169, Byte), Integer), CType(CType(169, Byte), Integer), CType(CType(169, Byte), Integer))
-        Me.btnSendAll.DisabledState.ForeColor = System.Drawing.Color.FromArgb(CType(CType(141, Byte), Integer), CType(CType(141, Byte), Integer), CType(CType(141, Byte), Integer))
-        Me.btnSendAll.FillColor = System.Drawing.Color.Transparent
-        Me.btnSendAll.Font = New System.Drawing.Font("Segoe UI", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.btnSendAll.ForeColor = System.Drawing.Color.FromArgb(CType(CType(36, Byte), Integer), CType(CType(35, Byte), Integer), CType(CType(34, Byte), Integer))
-        Me.btnSendAll.Location = New System.Drawing.Point(814, 3)
-        Me.btnSendAll.Name = "btnSendAll"
-        Me.btnSendAll.Size = New System.Drawing.Size(89, 40)
-        Me.btnSendAll.TabIndex = 21
-        Me.btnSendAll.Text = "Send All"
-        Me.btnSendAll.Visible = False
         '
         'deptChecklist
         '
---
+++ Deped Tracking System/deptSend.vb
@@ -0,0 +1,101 @@
+﻿Imports System.Data.OleDb
+Imports System.Net.Mail
+
+Public Class deptSend
+
+    Private Sub deptSend_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        dgvSelected.Rows.Clear()
+
+        For Each card In selectedCards
+            dgvSelected.Rows.Add(card.ControlNum, card.Title)
+        Next
+    End Sub
+
+    Private Sub btnSend_Click(sender As Object, e As EventArgs) Handles btnSend.Click
+        If cmbDepartment.SelectedIndex = -1 Then
+            MessageBox.Show("Please select a department.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            Return
+        End If
+
+        Dim newDept As String = cmbDepartment.SelectedItem.ToString()
+
+        Using con As New OleDbConnection(conString)
+            con.Open()
+
+            For Each card In selectedCards
+                ' 1. Get current info from DB (email, current dept, etc.)
+                Dim querySelect As String = "SELECT client_email, current_department 
+                                         FROM Documents 
+                                         WHERE control_num = @controlNum"
+
+                Dim clientEmail As String = ""
+                Dim prevDept As String = ""
+
+                Using cmdSelect As New OleDbCommand(querySelect, con)
+                    cmdSelect.Parameters.AddWithValue("@controlNum", card.ControlNum)
+                    Using reader As OleDbDataReader = cmdSelect.ExecuteReader()
+                        If reader.Read() Then
+                            clientEmail = reader("client_email").ToString()
+                            prevDept = reader("current_department").ToString()
+                        End If
+                    End Using
+                End Using
+
+                ' 2. Update DB with new department
+                Dim queryUpdate As String = "UPDATE Documents 
+                                         SET previous_department = @prevDept, 
+                                             current_department = @newDept 
+                                         WHERE control_num = @controlNum"
+
+                Using cmdUpdate As New OleDbCommand(queryUpdate, con)
+                    cmdUpdate.Parameters.AddWithValue("@prevDept", prevDept)
+                    cmdUpdate.Parameters.AddWithValue("@newDept", newDept)
+                    cmdUpdate.Parameters.AddWithValue("@controlNum", card.ControlNum)
+                    cmdUpdate.ExecuteNonQuery()
+                End Using
+
+                ' 3. Send Email Notification
+                If clientEmail <> "" Then
+                    Try
+                        Dim subject As String = "Transaction Update Notification"
+                        Dim body As String = $"Dear Client," & vbCrLf & vbCrLf &
+                                             $"Your transaction [{card.Title}] with control number [{card.ControlNum}] " &
+                                             $"has been sent from {prevDept} to {newDept}." & vbCrLf & vbCrLf &
+                                             "Thank you."
+
+                        SendEmail(clientEmail, subject, body)
+                    Catch ex As Exception
+                        MessageBox.Show($"Failed to send email for transaction {card.ControlNum}: {ex.Message}")
+                    End Try
+                End If
+            Next
+        End Using
+
+        MessageBox.Show("Documents sent successfully and emails have been dispatched.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        Me.Close()
+    End Sub
+
+    Private Sub SendEmail(toEmail As String, subject As String, body As String)
+        Dim smtp As New SmtpClient("smtp.yourserver.com") ' e.g. smtp.gmail.com
+        smtp.Port = 587
+        smtp.Credentials = New Net.NetworkCredential("youremail@domain.com", "yourpassword")
+        smtp.EnableSsl = True
+
+        Dim mail As New MailMessage()
+        mail.From = New MailAddress("youremail@domain.com")
+        mail.To.Add(toEmail)
+        mail.Subject = subject
+        mail.Body = body
+
+        smtp.Send(mail)
+    End Sub
+
+
+    Private selectedCards As List(Of creativeChecklist)
+
+    Public Sub New(cards As List(Of creativeChecklist))
+        InitializeComponent()
+        selectedCards = cards
+    End Sub
+
+End Class
